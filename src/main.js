8fcdd66081992b3f24bd9720720b10ce3b095468
mo.yu <mo.yu@tendcloud.com>
2016-10-07 09:05:20 +0800

    提交1

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from './router'
 Vue.config.productionTip = false
 
 
-console.log("hahaa11122222222333333322")
+console.log("hahaa1112222222233333332244444")
 
 /* eslint-disable no-new */
 new Vue({
